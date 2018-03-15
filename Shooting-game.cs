4d746896c6fbdd0061ee2c75f1b8ede2697c8354
Filip Vabrousek <filipvabrousek1@gmail.com>
2018-03-15 13:00:56 +0100

    Update and rename GM.cs to Shooting-game.cs

---
+++ Shooting-game.cs
@@ -0,0 +1,37 @@
+
+           
+            double sum = 0, average = 0, numberOfShots = 0;
+            int shot1 = 0;
+            Console.WriteLine("Vítejte ve střelecké hře");
+            Console.WriteLine("Zadejte hodnoty jednotlivých střel. Pro ukončení zadej číslo 11");
+
+
+            int ans2 = 0;
+            string answer = "a";
+            while (answer == "a")
+            {
+
+                if (ans2 != 11  /* answer != "n" && */ /*shot1 != 11*/)
+                {
+                    Console.WriteLine("Zadejte hodnotu střely");
+                    shot1 = int.Parse(Console.ReadLine());
+                   
+                    if (shot1 != 11) { 
+                    numberOfShots += 1;
+                    sum += shot1;
+                    Console.WriteLine("Hodnota střely " + numberOfShots + "je " + shot1);
+                    } else
+                    {
+                        answer = "n";
+                    }
+
+                   
+
+                }
+
+
+                average = sum / numberOfShots;
+                Console.WriteLine("Průměr je: " + average);
+            }
+
+            
