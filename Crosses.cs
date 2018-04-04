b2d6866d676913cffa9c00507b99c16335114ac7
Filip Vabrousek <filipvabrousek1@gmail.com>
2018-04-04 22:08:02 +0200

    Update Crosses.cs

--- Crosses.cs
+++ Crosses.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,114 +6,116 @@ using System.Threading.Tasks;
 
 
 
-namespace avga
-{
-    class Programa
-    {
-        static void Main(string[] args)
-        {
-            int player = 1, score1 = 0, score2 = 0, index = 0, place = 0;
-            int circlei = 0, crossi = 0, iterations = 0;
-            int[] fields = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
-            //List<int> taken = new List<int>();
+namespace avga {
+	class Programa {
+		static void Main(string[] args) {
+			int player = 1, score1 = 0, score2 = 0, index = 0, place = 0;
+			int circlei = 0, crossi = 0, iterations = 0;
+			int[] fields = new int[] {1,2,3,4,5,6,7,8,9};
 
-            Console.WriteLine("Vítejte ve hře piškvorky. Hrají křížky.");
-   
-            string ans = "a", choice = "A";
+			Console.WriteLine("Vítejte ve hře piškvorky. Hrají křížky.");
 
-            while (ans == "a"){
-            Console.WriteLine("Hraje hráč číslo " + player + " zadejte pozici na kterou chcete přidat váš symbol.");
-            place = Console.ReadLine(); 
-            
+			string ans = "a", choice = "A";
 
-            for (var i = 0; i < 9; i++){
-                if (place == taken[i]){
-                    Console.WriteLine("Toto pole je zabrané.");
-                } else {
-                    if (player == 1){
-                fields[place] = "X";
-                taken[place] = desired;
-                    }
-                    
-                }
-            }
-             
-        
-
-             if (player == 1) {player = 2;} else {player = 1}
+			while (ans == "a") {
+				Console.WriteLine("Hraje hráč číslo " + player + " zadejte pozici na kterou chcete přidat váš symbol.");
+				place = Console.ReadLine();
 
 
-            if (checkWin() == "true"){
-            Console.WriteLine("Někdo vyhrál.")
-            plot();
-            } else {
-                plot();
-            }
-             if (fields.length == 8){ ans == "n"}
+				for (var i = 0; i < 9; i++) {
+					if (place == taken[i]) {
+						Console.WriteLine("Toto pole je zabrané.");
+					} else {
+						if (player == 1) {
+							fields[place] = "X";
+							taken[place] = desired;
+						}
+
+					}
+				}
 
 
-            }
+
+				if (player == 1) {
+					player = 2;
+				} else {
+					player = 1
+				}
 
 
-  
-     
-
-     static void plot(){
-         Console.WriteLine(" | |")
-         Console.WriteLine(fields[0], fields[1], fields[2])
-         Console.WriteLine(fields[3], fields[4], fields[5])
-         Console.WriteLine(fields[6], fields[7], fields[8])
-         Console.WriteLine(" | |");
-     }
-// Console.WriteLine(fields[0], fields[1]);
-
-bool IsLine(int index0, int index1, int index2, string piece){
-return fields[index0] == piece && fields[index1] == piece && fields[index2] == piece;
-    }
+				if (checkWin() == "true") {
+					Console.WriteLine("Někdo vyhrál.")
+					plot();
+				} else {
+					plot();
+				}
+				if (fields.length == 8) {
+					ans == "n"
+				}
 
 
-bool IsAnyLine(int index0, int index1, int index2){
-        return IsLine(index0, index1, index2, Pos[index0]);
-    }
-
-
-static checkWin(){
-// horizontal
-if(IsAnyLine(1, 2, 3)) {
-return true;
-        }
-        if(IsAnyLine(4, 5, 6)){
-            return true;
-        }
-        if(IsAnyLine(7, 8, 9)){
-            return true;
-        }
-
-// Diagonal
-        if(IsAnyLine(1, 5, 9)){
-            return true;
-        }
-        if(IsAnyLine(7, 5, 3)){
-            return true;
-        }
-
-// Columns
-        if(IsAnyLine(1, 4, 7)){
-            return true;
-        }
-        if(IsAnyLine(2, 5, 8)){
-            return true;
-        }
-        if(IsAnyLine(3, 6, 9)){
-            return true;
-        }
-
-        return false;
-}
+			}
 
 
 
 
-        }
-    }
+
+			static void plot() {
+				Console.WriteLine(" | |")
+				Console.WriteLine(fields[0], fields[1], fields[2])
+				Console.WriteLine(fields[3], fields[4], fields[5])
+				Console.WriteLine(fields[6], fields[7], fields[8])
+				Console.WriteLine(" | |");
+			}
+			// Console.WriteLine(fields[0], fields[1]);
+
+			bool IsLine(int index0, int index1, int index2, string piece) {
+				return fields[index0] == piece && fields[index1] == piece && fields[index2] == piece;
+			}
+
+
+			bool IsAnyLine(int index0, int index1, int index2) {
+				return IsLine(index0, index1, index2, Pos[index0]);
+			}
+
+
+			static checkWin() {
+				// horizontal
+				if (IsAnyLine(1, 2, 3)) {
+					return true;
+				}
+				if (IsAnyLine(4, 5, 6)) {
+					return true;
+				}
+				if (IsAnyLine(7, 8, 9)) {
+					return true;
+				}
+
+				// Diagonal
+				if (IsAnyLine(1, 5, 9)) {
+					return true;
+				}
+				if (IsAnyLine(7, 5, 3)) {
+					return true;
+				}
+
+				// Columns
+				if (IsAnyLine(1, 4, 7)) {
+					return true;
+				}
+				if (IsAnyLine(2, 5, 8)) {
+					return true;
+				}
+				if (IsAnyLine(3, 6, 9)) {
+					return true;
+				}
+
+				return false;
+			}
+
+
+
+
+		}
+	}
 }
