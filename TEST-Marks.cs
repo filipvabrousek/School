8161b99682b214f3dabf475469c5c8aec444e384
Filip Vabrousek <filipvabrousek1@gmail.com>
2018-04-19 12:42:02 +0200

    Update TEST-Marks.cs

--- TEST-Marks.cs
+++ TEST-Marks.cs
@@ -1,111 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp8
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int radek, sloupec, r, s, pom;
-            double soucet, prumer;
+            
+            double sum = 0, res = 0;
+            Console.WriteLine("Zadejte délku pole.");
+            int maxLen = int.Parse(Console.ReadLine());
 
-            radek = 5;
-            sloupec = 5;
-            r = 0;
-            s = 0;
-            soucet = 0;
-
-            int[,] pole = new int[sloupec, radek];
-
-            Console.WriteLine("Ahoj :)");
-            Console.WriteLine("Jsem program.");
-            Console.WriteLine("Vypocitam tvuj prumer znamek.");
-
-            for (r = 0; r < radek; r++)
+            while (maxLen > 20)
             {
-                for (s = 0; s < sloupec; s++)
-                {
-                    pole[s, r] = int.Parse(Console.ReadLine());
-                }
+                Console.WriteLine("Zadejte menší číslo");
             }
+            int[] arr = new int[maxLen];
+            /* */
 
-            for (r = 0; r < radek; r++)
+            for (int i = 0; i < maxLen; i++)
             {
-                for (s = 0; s < sloupec; s++)
-                {
-                    Console.Write(pole[s, r]);
-                }
-                Console.WriteLine();
+                Console.WriteLine("Zadejte známku");
+                arr[i] = int.Parse(Console.ReadLine());
+                sum += arr[i];
+
             }
-
-
-            Console.WriteLine("Zadej znamky z predmetu v tomhle poradi: matematika, cesky jazyk, anglicky jazyk, fyzika, grafika.");
-
-            Console.WriteLine("Matematika:");
-            for (s = 0; s < sloupec; s++)
-            {
-                pom = pole[s, 0];
-                soucet = soucet + pom;
-            }
-            prumer = soucet / sloupec;
-            Console.WriteLine(prumer);
-            prumer = 0;
-            soucet = 0;
-            pom = 0;
-
-            Console.WriteLine("Cesky jazyk:");
-            for (s = 0; s < sloupec; s++)
-            {
-                pom = pole[s, 1];
-                soucet = soucet + pom;
-            }
-            prumer = soucet / sloupec;
-            Console.WriteLine(prumer);
-            prumer = 0;
-            soucet = 0;
-            pom = 0;
-
-            Console.WriteLine("Anglicky jazyk:");
-            for (s = 0; s < sloupec; s++)
-            {
-                pom = pole[s, 2];
-                soucet = soucet + pom;
-            }
-            prumer = soucet / sloupec;
-            Console.WriteLine(prumer);
-            prumer = 0;
-            soucet = 0;
-            pom = 0;
-
-            Console.WriteLine("Fyzika:");
-            for (s = 0; s < sloupec; s++)
-            {
-                pom = pole[s, 3];
-                soucet = soucet + pom;
-            }
-            prumer = soucet / sloupec;
-            Console.WriteLine(prumer);
-            prumer = 0;
-            soucet = 0;
-            pom = 0;
-
-            Console.WriteLine("Grafika:");
-            for (s = 0; s < sloupec; s++)
-            {
-                pom = pole[s, 4];
-                soucet = soucet + pom;
-            }
-            prumer = soucet / sloupec;
-            Console.WriteLine(prumer);
-            prumer = 0;
-            soucet = 0;
-            pom = 0;
+            res = sum / maxLen;
+            Console.WriteLine("Průměr je " + res);
 
             Console.ReadKey();
-        }
-    }
-}
