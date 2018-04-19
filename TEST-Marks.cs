9456e8501110fee428d65b2ec5f90a94c1539c35
Filip Vabrousek <filipvabrousek1@gmail.com>
2018-04-19 12:47:28 +0200

    Update TEST-Marks.cs

--- TEST-Marks.cs
+++ TEST-Marks.cs
@@ -22,3 +22,29 @@
             Console.WriteLine("Průměr je " + res);
 
             Console.ReadKey();
+/*
+         /*
+            // deklarace
+            int[, ] kinosal = new int[5, 5];
+            // naplnění daty
+            kinosal[2, 2] = 1; // Prostředek
+
+            for (int i = 1; i < 4; i++) // 4. řádek
+            {
+             kinosal[i, 3] = 1;
+            }
+            for (int i = 0; i < 5; i++) // Poslední řádek
+            {
+             kinosal[i, 4] = 1;
+            }
+
+            for (int j = 0; j < kinosal.GetLength(1); j++) {
+             for (int i = 0; i < kinosal.GetLength(0); i++) {
+              Console.Write(kinosal[i, j]);
+             }
+             Console.WriteLine();
+            }
+             
+             */
+
+*/
