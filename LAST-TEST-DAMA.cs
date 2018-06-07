46f0f328afc269c0b257de14a58194ac6d796358
Filip Vabrousek <filipvabrousek1@gmail.com>
2018-06-07 14:05:23 +0200

    Update LAST-TEST-DAMA.cs

--- LAST-TEST-DAMA.cs
+++ LAST-TEST-DAMA.cs
@@ -9,11 +9,7 @@ namespace test
         {
 
 
-            /*
-            Kinosál zadat (10 x 10)
-            Zadat kolik lidí vedle sebe chcete ? (3)
-            v array arr zjistí zda jsou 3 volná vedle sebe            
-            */
+
 
           
             int xp = 0;
@@ -21,7 +17,11 @@ namespace test
             int count = 3;
             int id = 0;
             int size = 4; // 4 - 8
-            int player = 1;
+            int player = 2;
+            int oneindex = 3;
+            int zeroindex = 3;
+            int wx = 0;
+            int wy = 0;
 
             string[,] seats = new string[0, 0];
 
@@ -29,8 +29,11 @@ namespace test
             Console.WriteLine("Dobrý den, vítá vás program Dáma");
             Console.WriteLine("Jak velké chcete hrací pole? povoleny jsou pouze hodnoty v rozmezí 4 - 8");
            size = int.Parse(Console.ReadLine());
+            Console.WriteLine("Kolik chcete povolit tahů ?");
+            count = int.Parse(Console.ReadLine());
 
-           
+
+
             if (size >= 4 && size <= 8){
                
                 seats = new string[size, size];
@@ -45,15 +48,25 @@ namespace test
                 {
                     //  id = id + 1;
                     if (player == 1) { player = 2; } else { player = 1; }
-                    Console.WriteLine("Na jakou pozici chcete posunout váš kámen. hraje hráč číslo " + player);
+
+
+                    Console.WriteLine("Vyberte x pozici kamene se kterým chcete pohnout? hraje hráč číslo " + player);
+                    wx = int.Parse(Console.ReadLine()) - 1;
+                    Console.WriteLine("Vyberte y pozici kamene se kterým chcete pohnout");
+                    wx = int.Parse(Console.ReadLine()) - 1;
+
+
+
+
+                    Console.WriteLine("Na jakou pozici chcete posunout váš kámen?");
                     Console.WriteLine("Zadejte řadu"); //2*2, 1*1
                     yp = int.Parse(Console.ReadLine()) - 1;
                     Console.WriteLine("Zadejte sloupec."); //2*2, 1*1
                     xp = int.Parse(Console.ReadLine()) - 1;
 
 
-
-                    moveStone(xp, yp, player, seats);
+                
+                    moveStone(xp, yp, player, seats, wx, wy);
 
                     writeSeats(size, size, seats);
                     //request(xp, yp, size, id, seats);
@@ -66,10 +79,10 @@ namespace test
 
 
 
-        static void moveStone(int x, int y, int player, string[,] arr){
+        static void moveStone(int x, int y, int player, string[,] arr, int wx, int wy){
          
             var res = Convert.ToString(player);
-            // pozice existuje
+  
             // protivník
             string contra = "";
                
@@ -79,12 +92,21 @@ namespace test
                 contra = "1";
                 }
 
-
+            // chci pohnout s kamenem
+      
 
             if (arr[x, y] == "O" || arr[x, y] == "1" || arr[x, y] == "2"){
 
                 Console.WriteLine("Jsem zde");
-            
+
+                if (arr[wx, wx] == res){
+                    Console.WriteLine("Můžete táhnout.");
+                    arr[wx, wy] = "O";
+                } else {
+                    Console.WriteLine("Na této pozici nemáte kámen.");
+                }
+
+
 
                 if (arr[x, y] == contra){
                     Console.WriteLine("Obsadil jste protivníka.");
@@ -105,23 +127,24 @@ namespace test
       
 
 
-
+        // vyplnění první řady 0
         static string[,] fillSeatsf(int count, int id, string[,] arr)
         {
             for (var i = 0; i < count; i++){
                 arr[i, 0] = Convert.ToString(id);
-                //arr[0, count - 1] = Convert.ToString(id);
+       
             }
             return arr;
         }
 
 
+        // vyplnění druhé řady 1
         static string[,] fillSeatsl(int count, int id, string[,] arr)
         {
             for (var i = 0; i < count; i++)
             {
                 arr[i, count - 1] = Convert.ToString(id);
-                //arr[0, count - 1] = Convert.ToString(id);
+
             }
             return arr;
         }
