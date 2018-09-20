a998cc8022c1e04025e461e8e5402c9585871fc1
Filip Vabrousek <filipvabrousek1@gmail.com>
2018-09-20 11:48:00 +0200

    Update 3D-Ships.cs

--- CIA/3D-Ships.cs
+++ CIA/3D-Ships.cs
@@ -1,59 +1,46 @@
-// 26.4.2018 
- 
- /*nefunguje mi zadávání lodí přes 2 pole, lze zadat jen jako souřadnici x, y
+// 26.4.2018
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+/*nefunguje mi zadávání lodí přes 2 pole, lze zadat jen jako souřadnici x, y
  vyhodnocení funguje jako porovnání souřadnic požadových střel a zásahů */
 
 
             int x, y, r, s, pom;
             double sum, avg, percent;
 
-            x = 10;
-            y = 10;
+            x = 3;
+            y = 3;
             r = 0;
             s = 0;
             sum = 0;
             percent = 0;
             int success = 0;
             int fail = 0;
-            // char[,] pole = new char[sloupcu, radku];
-            int xPosition = 0;
-            int yPosition = 0;
-            char[,] arr = new char[x, y];
-            int[,] shot = new int[y, x];
 
 
-           
+            int[,] arr = new int[y, x];
+
+            Console.WriteLine("Zadejte pozice vašich lodí v pořadí x, y");
 
             for (r = 0; r < x; r++)
             {
                 for (s = 0; s < y; s++)
                 {
-                    arr[s, r] = 'O'; // "."
+                    arr[s, r] = int.Parse(Console.ReadLine());
                 }
             }
-
-            /*--------------------------------- SHIP 1 --------------------------------- */
-            Console.WriteLine("Zadejte X pozici  lodě 1 (0-10)"); //2*2, 1*1
-            xPosition = int.Parse(Console.ReadLine());
-            Console.WriteLine("Zadejte Y pozici  lodě 1"); //2*2, 1*1
-            yPosition = int.Parse(Console.ReadLine());
-            arr[xPosition, yPosition] = 'X'; // "."
-
-
-
-            Console.WriteLine("Zadejte X pozici  lodě 2"); //2*2, 1*1
-            xPosition = int.Parse(Console.ReadLine());
-            Console.WriteLine("Zadejte Y pozici  lodě 2"); //2*2, 1*1
-            yPosition = int.Parse(Console.ReadLine());
-            arr[xPosition, yPosition] = 'X'; // "."
-
-
-            Console.WriteLine("Zadejte X pozici  lodě 3"); //2*2, 1*1
-            xPosition = int.Parse(Console.ReadLine());
-            Console.WriteLine("Zadejte Y pozici  lodě 3"); //2*2, 1*1
-            yPosition = int.Parse(Console.ReadLine());
-            arr[xPosition, yPosition] = 'X'; 
-            arr[xPosition + 1 , yPosition + 1] = 'X'; // 2 fields
 
             for (r = 0; r < x; r++)
             {
@@ -65,31 +52,38 @@
             }
 
 
-
             Console.WriteLine("Kam chcete vystřelit ? Zadejte všechny souřadnice na které chcete vystřelit");
-            // int[,] shot = new int[y, x];
-
-            Console.WriteLine("Zadejte X pozici  výstřelu"); //2*2, 1*1
-            xPosition = int.Parse(Console.ReadLine());
-            Console.WriteLine("Zadejte Y pozici  výstřelu"); //2*2, 1*1
-            yPosition = int.Parse(Console.ReadLine());
-            shot[xPosition, yPosition] = 'O'; // výstřel
-
-            // check win
+            int[,] shot = new int[y, x];
             for (r = 0; r < x; r++)
             {
                 for (s = 0; s < y; s++)
                 {
-                    if (arr[s, r] == shot[xPosition, yPosition])
-                    {
+                    shot[s, r] = int.Parse(Console.ReadLine());
+                }
+            }
+
+          
+
+
+
+
+
+            // zkontrolovat výhru
+            for (r = 0; r < x; r++)
+             {
+                 for (s = 0; s < y; s++)
+                 {
+                     if (arr[s, r] == shot[s, r])
+                     {
+                         Console.Write(" Zásah na souřadnici " + arr[s, r]);
                         success += 1;
-                    }
-                    else
+                     } else
                     {
+                        Console.Write("Mimo :)");
                         fail += 1;
                     }
 
-                    if (success > fail)
+                     if (success > fail)
                     {
                         percent = (fail / success) * 100;
                     }
@@ -98,8 +92,8 @@
                         percent = (success / fail) * 100;
                     }
                 }
-                Console.WriteLine();
-            }
+                 Console.WriteLine();
+             }
 
 
 
@@ -107,3 +101,8 @@
             Console.WriteLine("Zasáhli jste " + success + " minuli jste " + fail + " zasáhli jste " + percent + " lodí" + " minuli jste " + (100 - percent) + " % lodí");
 
             Console.ReadKey();
+
+
+        }
+    }
+}
