d511f4070801aa8040a4f889c77933277d646160
Filip Vabrousek <filipvabrousek1@gmail.com>
2018-09-20 11:42:29 +0200

    Create 3D-Hangman.cs

---
+++ CIA/3D-Hangman.cs
@@ -0,0 +1,38 @@
+            string text = ""; // use enters the string
+
+            int tries = 0;
+            char guess = 'O';
+
+            Console.WriteLine("Zadejte text");
+            text = Console.ReadLine(); // string
+            char[] empty = new char[text.Length]; // string where will be our guess
+            Console.WriteLine("Zadejte počet pokusů (tolik jako písmen. Pokud slovo obsahuje písmeno 2x nebo vícekrát počítá se jako jedno");
+            Console.WriteLine("tzv. na slove Filip stačí 4 pokusy");
+            tries = int.Parse(Console.ReadLine());
+            int correct = 0;
+
+            for (var i = 0; i < tries; i++)
+            {
+                Console.WriteLine("Zadejte písmeno");
+
+                guess = char.Parse(Console.ReadLine());
+                for (var n = 0; n < text.Length; n++)
+                {
+                    if (text[n].ToString().ToUpper() == guess.ToString().ToUpper())
+                    {
+                        empty[n] = guess;
+                        correct += 1;
+                        if (correct == text.Length) {
+                            Console.WriteLine("Vyhráli jste!");
+                    }
+                    }
+
+                }
+
+            
+                Console.WriteLine(new string(empty));
+
+            }
+
+       
+            Console.ReadKey();
