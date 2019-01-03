d199d01e293568c253540362917a56a7283030ab
Filip Vabrousek <filipvabrousek1@gmail.com>
2019-01-03 12:23:24 +0100

    Update 4D-Morse.cs

--- CIA/4D-Morse.cs
+++ CIA/4D-Morse.cs
@@ -12,7 +12,7 @@ namespace MorseCode
             string[] alphabet = { "A", "B", "C", "D", "E", "F", "G", "H", "CH", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " " };
             string[] morse = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "----", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", " " };
             // .-||-...||-.-.||-..||.||..-.||--.||....||----||..||.---||-.-||.-..||--||-.||---||.--.||--.-||.-.||...||-||..-||...-||-..-||-.--||--..|"
-
+            string name = "";
             /*
 
 1 - Create the file
@@ -43,26 +43,40 @@ namespace MorseCode
 
 
 
-                Console.WriteLine("Zadejte název souboru ve kořenové složce projektu");
+                Console.WriteLine("Zadejte název souboru ve kořenové složce projektu bez txt");
 
                 // /Users/filipvabrousek/Projects/MorseCode/MorseCode/bin/Debug/
-                var name = Console.ReadLine();
-                var path = Directory.GetCurrentDirectory() + "/" + name; 
+                name = Console.ReadLine();
+                var path = Directory.GetCurrentDirectory() + "/" + name + ".txt"; 
                
 
 
                 Console.WriteLine("Je soubor v morseovce (8) nebo v textu (9)");
                  dec = int.Parse(Console.ReadLine());
 
+                // CHECK THE PATH 
+                // var path = Directory.GetCurrentDirectory() + "/" + nameb; 
+                var patha = Directory.GetCurrentDirectory() + "/" + name + ".txt";
+                if (File.Exists(patha)){
+                    Console.WriteLine("Soubor " + name + "již existuje. Chcete jej přepsat ? (a / n)");
+
+                    var ans = Console.ReadLine();
+                    if (ans == "a"){
+                        Console.WriteLine("Soubor bude přepsán"); 
+                    } else {
+                        Console.WriteLine("Zadejte prosím jiný název. (bez .txt)");
+                        name = Console.ReadLine();
+                    }
+                } 
 
                
                 if (File.Exists(path)){
-                    StreamReader sr = new StreamReader(path);
+                    StreamReader sr = new StreamReader(path); // here we can define our custom path
                     string contents = sr.ReadToEnd();
                     text = contents;
                     Console.WriteLine("Contents are " + contents);
                 } else {
-                    Console.WriteLine("File doesn't exist. DEFAULT text was used.");
+                    Console.WriteLine("Soubor neexistuje.");
                     text = "DEFAULT";
                 }
 
@@ -77,14 +91,34 @@ namespace MorseCode
 
 
 
+                Console.WriteLine("Zadejte název souboru do kterého bude text zapsán (bez .txt)");
 
-                Console.WriteLine("3 - Convert text 2 morse code. ");
-                Console.WriteLine("4 - Convert morse code 2 text ");
+                 name = Console.ReadLine();
+
+
+                // CHECK THE PATH 
+                // var path = Directory.GetCurrentDirectory() + "/" + nameb; 
+                var patha = Directory.GetCurrentDirectory() + "/" + name;
+                if (File.Exists(patha)){
+                    Console.WriteLine("Soubor " + name + "již existuje. Chcete jej přepsat ? (a / n)");
+
+                    var ans = Console.ReadLine();
+                    if (ans == "a"){
+                        Console.WriteLine("Soubor bude přepsán"); 
+                    } else {
+                        Console.WriteLine("Zadejte prosím jiný název. (bez .txt)");
+                        name = Console.ReadLine();
+                    }
+                } 
+
+
+                Console.WriteLine("3 - Převod textu do morseovky. ");
+                Console.WriteLine("4 - Převod morseovky do textu. ");
                 opt = int.Parse(Console.ReadLine());
 
 
                 if (opt == 3){
-                    Console.WriteLine("Enter text you would like to convert to morse code");
+                    Console.WriteLine("Zadejte text, který chcete převést do morseovky.");
                     text = Console.ReadLine();
                 }
 
@@ -134,7 +168,7 @@ namespace MorseCode
 
                 StreamWriter sw = null;
 
-                sw = new StreamWriter("morse-B.txt");
+                sw = new StreamWriter(name);
 
                 for (var i = 0; i < res.Length; i++)
                 {
@@ -144,11 +178,16 @@ namespace MorseCode
                 sw.Close();
 
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(text + " to morse is " + res);
+                Console.WriteLine(text + " v morseovce " + res);
                 Console.ForegroundColor = ConsoleColor.White;
 
+
+
+
+
+
         
-                Console.WriteLine("Text byl převeden do morseovky a zapsán do souboru morseB.txt");
+                Console.WriteLine("Text byl převeden do morseovky a zapsán do souboru " + name + ".txt");
       
 
 
@@ -221,12 +260,12 @@ namespace MorseCode
                 }
 
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(everystr + " to text is " + resa);
+                Console.WriteLine(everystr + " v textu " + resa);
                 Console.ForegroundColor = ConsoleColor.White;
 
                 // Write to file
 
-                sw = new StreamWriter("morse-A.txt");
+                sw = new StreamWriter("" + name + "");
 
                 for (var i = 0; i < resa.Length; i++)
                 {
@@ -236,7 +275,7 @@ namespace MorseCode
                 sw.Close();
 
 
-                Console.WriteLine("Morseovka byla převedena do textu a zapsána do souboru morse-A.txt");
+                Console.WriteLine("Morseovka byla převedena do textu a zapsána do souboru " + name + ".txt");
                
 
             }
