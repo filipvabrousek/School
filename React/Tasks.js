99f14b5f73891fd796ccb63b0f9820edc1c82dec
Filip Vabrousek <filipvabrousek1@gmail.com>
2020-03-24 18:23:02 +0100

    Update Tasks.js

--- React/Tasks.js
+++ React/Tasks.js
@@ -3,52 +3,36 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 // To-Do List
-class Input extends React.Component {
+// 23/03/20 - new begin
+// 24/03/20 - 17:57:00 Tasks do list
+
+class Tasks extends React.Component {
     constructor(props){
         super(props);
-    }
+		this.state = {value: "", tasks: []}
+   	 }
+	
+	onChange = (e) => {
+		this.setState({value: e.target.value});
+	}
    
-	add(){	
-		console.log("Item added.")
+	add = (e) => {	
+		this.state.tasks.push(this.state.value);
+		this.setState({ tasks: this.state.tasks });
 	}
 	
     render(){
         return (
             <div>
-            <h1>Tasks</h1>
-                <input type="text" placeholder="Add a task"/><br></br>
+            <input type="text" placeholder="Add a task" onChange={this.onChange}/><br></br>
             <button onClick={this.add}>Add a task</button>
+		 {this.state.tasks.map(x => <h1>{`${x}`}</h1>)}
             </div>
         );
     }
 }
 
 
-class TaskList extends React.Component {
-    constructor(p){
-        super(p);
-        this.state = {tasks: ["Item 1", "Item 2", "Item 3"]}
-    }
-    
-    render(){
-       return this.state.tasks.map(n => <li>{n}</li>);
-    }
-}
-                                   
-         
-
-class Tasks extends React.Component {	
-		
-		render(){
-			return (
-				<div>
-				<Input/>
-				<TaskList/>
-				</div>
-			)
-		}
-}		
-                                
 ReactDOM.render(
 <Tasks/>,
 document.querySelector("#root")
